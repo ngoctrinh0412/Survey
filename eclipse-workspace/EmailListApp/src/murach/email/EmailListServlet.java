1f46beb8d7554142146f3b074afe45b6087c8f3b
Ngoc Trinh <trinhga0412@gmail.com>
2025-08-21 21:31:17 +0700

    commit code

---
+++ eclipse-workspace/EmailListApp/src/murach/email/EmailListServlet.java
@@ -0,0 +1,37 @@
+package murach.email;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class EmailListServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String url = "/index.html";   // mặc định quay lại form
+
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "join";
+        }
+
+        if (action.equals("join")) {
+            url = "/index.html";
+        } else if (action.equals("add")) {
+            String firstName = request.getParameter("firstName");
+            String lastName = request.getParameter("lastName");
+            String email = request.getParameter("email");
+
+            // gắn dữ liệu vào attribute
+            request.setAttribute("firstName", firstName);
+            request.setAttribute("lastName", lastName);
+            request.setAttribute("email", email);
+
+            url = "/thanks.jsp"; // chuyển đến trang cảm ơn
+        }
+
+        getServletContext().getRequestDispatcher(url).forward(request, response);
+    }
+}
